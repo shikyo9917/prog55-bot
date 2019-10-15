a73efbcca4373b47b6fb5c01a32cf6896bc17871
Skiiney <vihtorg@gmail.com>
2019-10-14 23:28:51 -0300

    a

--- bot.js
+++ bot.js
@@ -69,7 +69,7 @@ function resetPolling() {
     setPolling();
 }
 
-var prefix = 'k!'
+var prefix = '!'
 client.Dispatcher.on("MESSAGE_CREATE", e => {
     //reset polling since heroku is active
     // resetPolling();
